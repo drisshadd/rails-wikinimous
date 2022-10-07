ee37aed46813e71d7544a19be4789f6a221893b7
Driss Haddadi <drisshad2001@gmail.com>
2022-10-07 20:55:58 +0100

    Finished Website

---
+++ app/controllers/article_controller.rb
@@ -0,0 +1,40 @@
+class ArticleController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    # No need for app/views/articles/create.html.erb
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
